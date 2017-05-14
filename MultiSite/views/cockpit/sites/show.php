03fae59330682b7eb8baed2b52429aa0d00dcb55
ldandoy <ldandoy@overconsulting.net>
2017-05-14 22:54:56 +0200

    Correction

--- MultiSite/views/cockpit/sites/show.php
+++ MultiSite/views/cockpit/sites/show.php
@@ -3,23 +3,25 @@
     <div class="box-header">
         <h3 class="box-title">{{ blockTitle }}</h3>
         <div class="box-tools pull-right">
+            {% button url="cockpit_multisite_sites_edit_<?php echo $site->id; ?>" type="info" size="xs" icon="pencil" %}
+            {% button url="cockpit_multisite_sites_delete_<?php echo $site->id; ?>" type="danger" size="xs" icon="trash-o" confirmation="Vous confirmer vouloir supprimer ce site ?" %}
             <a href="<?php echo url('cockpit_multisite_sites_index'); ?>" class="btn btn-default btn-xs"><i class="fa fa-arrow-left"></i></a>
         </div>
     </div>
     <div class="box-body">
    		<p>
-   			<b>Label</b>: <?php echo $params['site']->label; ?>
+   			<b>Label</b>: <?php echo $site->label; ?>
    		</p>
    		<p>
-   			<b>Host</b>: <?php echo $params['site']->host; ?>
+   			<b>Host</b>: <?php echo $site->host; ?>
    		</p>
    		<p>
-    		<b>Description</b>: <?php echo $params['site']->description; ?>
+    		<b>Description</b>: <?php echo $site->description; ?>
     	</p>
     	<p>
     		<b>Actif</b>:
     		<?php
-    			if ($params['site']->active == 1) {
+    			if ($site->active == 1) {
         			echo '<span class="label label-success">Activé</span>';
     			} else {
         			echo '<span class="label label-danger">Désactivé</span>';
